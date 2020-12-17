b79d1f7ede7ca9087d96737d0a91a26580eac871
ErikSchults <erik.schults@pipedrive.com>
2020-12-17 18:29:12 +0200

    v1

---
+++ test/unit/jest.coverage.js
@@ -0,0 +1,18 @@
+module.exports = {
+	rootDir: '../..',
+	resetModules: true,
+	resetMocks: true,
+	coverageReporters: ['text', 'json', 'lcov', 'clover'],
+	collectCoverage: true,
+	collectCoverageFrom: ['<rootDir>/lib/**/*.js'],
+	coveragePathIgnorePatterns: [
+		'/node_modules/',
+		'/examples/',
+		'/coverage/',
+	],
+	testMatch: [
+		'<rootDir>/test/unit/**/*.test.js',
+		'<rootDir>/lib/**/*.test.js',
+	],
+	modulePaths: ['<rootDir>'],
+};
